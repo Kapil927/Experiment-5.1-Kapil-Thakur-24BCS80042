9132028be72a6bd698d0f4c14efe954372057685
Kapil Thakur <91180037+Kapil927@users.noreply.github.com>
2025-10-05 14:50:12 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -26,6 +26,21 @@ app.use((req, res, next) => {
   next();
 });
 
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*"); 
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS"); 
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  // Handle preflight request
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
+
+
+
 app.use(bodyParser.json());
 
 // API routes
